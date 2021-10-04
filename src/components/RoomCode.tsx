1f448c9975d914f77b9c9c52e659f577cda79bcc
John <john.gabriel@fatecitapetininga.edu.br>
2021-10-04 15:22:08 -0300

    projeto letmeask

---
+++ src/components/RoomCode.tsx
@@ -0,0 +1,28 @@
+import toast, {Toaster} from 'react-hot-toast'
+import copyImg from '../assets/images/copy.svg'
+
+import '../styles/room-code.scss'
+
+type RoomCodeProps = {
+  code: string;
+}
+export function RoomCode(props: RoomCodeProps){
+  function copyRoomCodeToClipboard(){
+    navigator.clipboard.writeText(props.code)
+    toast.success('Added to clipboard',{
+      position: 'bottom-right'
+    })
+  }
+
+  return(
+    <>
+      <button className="room-code" onClick={copyRoomCodeToClipboard}>
+        <div>
+          <img src={copyImg} alt="Copy room code"/>
+        </div>
+        <span>Sala #{props.code}</span>
+      </button>
+      <Toaster/>
+    </>
+  )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,11 @@
+import { FormEvent, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import illustration from '../assets/images/illustration.svg';
 import logo from '../assets/images/logo.svg';
 import googleIcon from '../assets/images/google-icon.svg';
+
+import { database } from '../services/firebase';
 
 import { Button } from '../components/Button';
 import { useAuth} from '../hooks/useAuth';
@@ -11,13 +14,37 @@ import '../styles/auth.scss';
 
 export function Home(){
   const history = useHistory();
-  const { user, singInWithGoogle} = useAuth()
+  const { user, singInWithGoogle} = useAuth();
+  const [roomCode, setRoomCode] = useState('');
 
   async function handleCreateRoom(){
     if (!user){
       await singInWithGoogle();
     }
     history.push('/rooms/new');
+  }
+
+  async function handleJoinRoom(event:FormEvent){
+    event.preventDefault();
+
+    if(roomCode.trim() === ''){
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if(!roomRef.exists()){
+      alert('Room does not exist.');
+      return;
+    }
+
+    if(roomRef.val().endedAt){
+      // modificar para apenas desabilitar form de perguntas
+      alert('Room already closed.');
+      return;
+    }
+
+    history.push(`/rooms/${roomCode}`);
   }
 
   return(
@@ -35,10 +62,12 @@ export function Home(){
             Crie sua sala com o Google
           </button>
           <div className="separator">ou entre em uma sala</div>
-          <form>
+          <form onSubmit={handleJoinRoom}>
             <input 
               type="text"
               placeholder="Digite o código da sala"
+              onChange={event => setRoomCode(event.target.value)}
+              value={roomCode}
             />
             <Button type="submit">
               Entrar na sala
